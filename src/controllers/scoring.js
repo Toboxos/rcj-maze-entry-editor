734a063ec51b149dbeb9cc6eb273f51863fd8145
Tobias Bungard <tobi-bungard@t-online.de>
2023-10-26 15:28:05 +0200

    Implemented scoring run on SchedulePage

---
+++ src/controllers/scoring.js
@@ -0,0 +1,280 @@
+import {reactive, computed, watch} from "vue";
+
+export function newScoring() {
+    const values = newValues()
+    const scores = {
+        reliabilityBonus: computed( () => {
+            return Math.max(0, 10 * (
+                values.numVicitimsDetected + values.numRescueKitsDeployed - values.numLackOfProgress
+            ))
+        }),
+        exitBonus: computed( () => {
+            return values.exitBonusAchieved ? values.numVicitimsDetected * 10 : 0
+        }),
+        points: computed( () => {
+            let _points = 0;
+
+            // checkpoints
+            _points += values.numCheckpointsReached * 10
+            _points += values.numCheckpointsBonus * 10
+
+            // bumper
+            _points += values.numBumpersPassed * 5
+
+            // victims
+            _points += values.numVicitimsDetected * 10
+            _points += values.numRescueKitsDeployed * 10
+
+            // ramp
+            _points += values.numRampUp * 20
+            _points += values.numRampDown * 10
+
+            // bonus
+            _points += scores.reliabilityBonus.value
+            _points += scores.exitBonus.value
+
+            return _points
+        })
+    }
+    const timeFormatted = computed(() => {
+        // time [s]
+        // formated time output : 00:00
+        const minutes = Math.floor(values.time / 1000 / 60);
+        const seconds = Math.floor(values.time / 1000) - minutes * 60;
+        return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    })
+    const actions = reactive([]);
+
+    let startTime = 0;
+    let timeUpdateInterval = null;
+
+    watch(actions, newActions => {
+        newActions.forEach( action => {
+            setTimeout( () => {
+                action.execute(action.tile)
+            }, 1)
+        })
+    })
+
+    return {
+        values: values,
+        scores: scores,
+        timeFormatted: timeFormatted,
+        actions: actions,
+
+        start() {
+            values.time = 0;
+            startTime = Date.now();
+
+            // start interval to update time periodically
+            timeUpdateInterval = setInterval( () => {
+                values.time = Date.now() - startTime;
+            }, 1000);
+
+            // push back start action
+            actions.push({
+                execute: actionStart(values),
+                tile: null,
+                description: 'Run started',
+                timestamp: Date.now()
+            });
+        },
+        stop() {
+            values.time = Date.now() - startTime;
+
+            // stop interval to update time periodically
+            if( timeUpdateInterval !== null ) {
+                clearInterval(timeUpdateInterval);
+                timeUpdateInterval = null;
+            }
+
+            actions.push({
+                execute: () => {},
+                tile: null,
+                description: 'Run stopped',
+                timestamp: Date.now()
+            });
+        },
+        checkpointReached(tile) {
+            actions.push({
+                execute: actionCheckpointReached(values),
+                tile: tile,
+                description: 'Checkpoint reached (10p)' + (values.numLackOfProgress ? ' + First try (10p)' : ''),
+                timestamp: Date.now()
+            });
+        },
+        checkpointSkipped(tile) {
+            actions.push({
+                execute: actionCheckpointSkipped(values),
+                tile: tile,
+                description: 'Checkpoint skipped',
+                timestamp: Date.now()
+            });
+        },
+        lackOfProgress() {
+            actions.push({
+                execute: actionLackOfProgress(values),
+                tile: null,
+                description: 'Lack of Progress',
+                timestamp: Date.now()
+            });
+        },
+        bumperPassed(tile) {
+            // push back bumper action
+            actions.push({
+                execute: actionBumperPassed(values),
+                tile: tile,
+                description: 'Bumper passed (5p)',
+                timestamp: Date.now()
+            });
+        },
+        victimDetected(tile) {
+            actions.push({
+                execute: actionVictimDetected(values),
+                tile: tile,
+                description: 'Victim detected (10p)',
+                timestamp: Date.now()
+            });
+        },
+        deployedRescueKit(tile) {
+            actions.push({
+                execute: actionRescueKitDeployed(values),
+                tile: tile,
+                description: 'Rescue Kit deployed (10p)',
+                timestamp: Date.now()
+            });
+        },
+        exitFound(tile) {
+            actions.push({
+                execute: actionExitBonus(values),
+                tile: tile,
+                description: 'Exit detected (20p)',
+                timestamp: Date.now()
+            });
+        },
+        rampUp(tile) {
+            actions.push({
+                execute: actionRampUp(values),
+                tile: tile,
+                description: 'Ramp Up (20p)',
+                timestamp: Date.now(),
+            });
+        },
+        rampDown(tile) {
+            actions.push({
+                execute: actionRampDown(values),
+                tile: tile,
+                description: 'Ramp Down (10p)',
+                timestamp: Date.now(),
+            });
+        },
+
+        undo() {
+            // pop back last action
+            actions.pop();
+        }
+    }
+}
+
+function newValues() {
+    return reactive({
+        time: 0,
+        numCheckpointsReached: 0,
+        numCheckpointsBonus: 0,
+        numLackOfProgress: 0,
+        numBumpersPassed: 0,
+        numVicitimsDetected: 0,
+        numRescueKitsDeployed: 0,
+        exitBonusAchieved: false,
+        numRampUp: 0,
+        numRampDown: 0,
+
+        currentLegWithoutLOP: true,
+    });
+}
+
+function actionStart(values) {
+    return () => {
+        values.numCheckpoints = 0
+        values.numCheckpointsBonus = 0
+        values.numLackOfProgress = 0
+        values.numBumpersPassed = 0
+        values.numVicitimsDetected = 0
+        values.numRescueKitsDeployed = 0
+        values.exitBonusAchieved = false
+        values.numRampUp = 0
+        values.numRampDown = 0
+
+        values.currentLegWithoutLOP = true
+    }
+}
+
+function actionCheckpointReached(values) {
+    return (tile) => {
+        tile.checkpointVisited = true;
+        values.numCheckpoints++;
+
+        if( values.currentLegWithoutLOP ) {
+            values.numCheckpointsWithBonus++;
+            tile.checkpointWithBonus = true;
+        }
+        values.currentLegWithoutLOP = true;
+    }
+}
+
+function actionCheckpointSkipped(values) {
+    return (tile) => {
+        values.currentLegWithoutLOP = true;
+        tile.checkpointVisited = true;
+        tile.checkpointSkipped = true;
+    }
+}
+
+function actionLackOfProgress(values) {
+    return () => {
+        values.numLackOfProgress++;
+        values.currentLegWithoutLOP = false;
+    }
+}
+
+function actionBumperPassed(values) {
+    return (tile) => {
+        values.numBumpersPassed++;
+        tile.bumperPassed = true;
+    }
+}
+
+function actionVictimDetected(values) {
+    return (tile) => {
+        values.numVictimsDetected++;
+        tile.victimDetected = true;
+    }
+}
+
+function actionRescueKitDeployed(values) {
+    return (tile) => {
+        values.numRescueKitsDeployed++;
+        tile.rescueKitDeployed = true;
+    }
+}
+
+function actionExitBonus(values) {
+    return (tile) => {
+        values.exitBonusAchieved = true;
+        tile.exitFound = true;
+    }
+}
+
+function actionRampUp(values) {
+    return (tile) => {
+        values.numRampUp++;
+        tile.rampUp = true;
+    }
+}
+
+function actionRampDown(values) {
+    return (tile) => {
+        values.numRampDown++;
+        tile.rampDown = true;
+    }
+}
--- src/pages/SchedulePage.vue
+++ src/pages/SchedulePage.vue
@@ -5,8 +5,50 @@
       <button class="btn" @click="back">Back</button>
     </div>
 
+    <!-- Playmode stats -->
+    <div>
+      <div>TeamName: {{ schedule.team }}</div>
+
+      <div>Stats:</div>
+      <div class="p-2 border-2 border-black">
+        <div>Time: {{ scoring.timeFormatted }}</div>
+        <div>Checkpoints (Bonus): {{ scoring.values.numCheckpointsReached }} ({{ scoring.values.numCheckpointsBonus }})</div>
+        <div>Bumpers passed: {{ scoring.values.numBumpersPassed }}</div>
+        <div>Victims detected / Rescue kits deployed: {{ scoring.values.numVicitimsDetected }} / {{ scoring.values.numRescueKitsDeployed }}</div>
+        <div>Lack of progress: {{ scoring.values.numLackOfProgress }}</div>
+        <div>Ramp up / Ramp down: {{ scoring.values.numRampUp }} / {{ scoring.values.numRampDown }}</div>
+      </div>
+
+      <div>Points:</div>
+      <div class="p-2 border-2 border-black">
+        <div>Reliability Bonus: {{ scoring.scores.reliabilityBonus }}</div>
+        <div>Exit Bonus: {{ scoring.scores.exitBonus }}</div>
+        <div>Total: {{ scoring.scores.points }}</div>
+      </div>
+    </div>
+
+    <!-- Playmode actions -->
+    <div>
+      <div>Actions:</div>
+      <div class="p-2 border-2 border-black space-x-2">
+        <button class="btn p-2" @click="scoring.start()">Start</button>
+        <button class="btn p-2" @click="scoring.stop()">Stop</button>
+        <button class="btn p-2" @click="scoring.lackOfProgress()">Lack of Progress</button>
+      </div>
+      <div class="p-2 border-2 border-black space-x-2" v-if="selectedTile">
+        <button class="btn p-2" @click="scoring.checkpointReached(selectedTile)" v-if="selectedTile.checkpoint && !selectedTile.checkpointVisited">Checkpoint reached</button>
+        <button class="btn p-2" @click="scoring.checkpointSkipped(selectedTile)" v-if="selectedTile.checkpoint && !selectedTile.checkpointVisited">Checkpoint skipped</button>
+        <button class="btn p-2" @click="scoring.bumperPassed(selectedTile)" v-if="selectedTile.bumper && !selectedTile.bumperPassed">Bumper passed</button>
+        <button class="btn p-2" @click="scoring.victimDetected(selectedTile)" v-if="selectedTile.victim && !selectedTile.victimDetected">Victim detected</button>
+        <button class="btn p-2" @click="scoring.deployedRescueKit(selectedTile)" v-if="selectedTile.victim && !selectedTile.rescueKitDeployed">Rescue Kit deployed</button>
+        <button class="btn p-2" @click="scoring.rampUp(selectedTile)" v-if="selectedTile.isRamp && !selectedTile.rampUp">Ramp Up</button>
+        <button class="btn p-2" @click="scoring.rampDown(selectedTile)" v-if="selectedTile.isRamp && !selectedTile.rampDown">Ramp Down</button>
+        <button class="btn p-2" @click="scoring.exitFound(selectedTile)" v-if="selectedTile.startpoint && !selectedTile.exitBonusAchieved">Exit-Bonus</button>
+      </div>
+    </div>
+
     <div class="mt-4">
-      <MazeField :maze="parcour.maze" :editable="false" @tileClicked="tileClicked"></MazeField>
+      <MazeField :maze="maze" :editable="false" @tileClicked="tileClicked"></MazeField>
     </div>
   </div>
 </template>
@@ -14,8 +56,10 @@
 <script setup>
 import {getScheduleById, useCompetitions} from "../stores/competitions.js";
 import {getParcourById, useParcours} from "../stores/Parcours.js";
+import {newScoring} from "../controllers/scoring.js";
 import MazeField from "../components/MazeField.vue";
 import {useRouter} from "vue-router";
+import {ref, watch} from "vue";
 
 useCompetitions() // make sure that they are initialized
 useParcours() // make sure that they are initialized
@@ -24,12 +68,73 @@ const router = useRouter()
 const props = defineProps(['competitionId', 'scheduleId'])
 const schedule = getScheduleById(parseInt(props.competitionId), parseInt(props.scheduleId))
 const parcour = getParcourById(schedule.parcourId)
+const maze = ref(JSON.parse(JSON.stringify(parcour.maze)))
+const scoring = newScoring();
+
+const selectedTile = ref(null);
+const victim = ref(false);
+const black = ref(false);
+const bumper = ref(false);
+const checkpoint = ref(false);
+const startpoint = ref(false);
+const ramp = ref(false);
+const editMode = ref(false);
+
+watch(victim, (newVal, oldVal) => {
+  if (selectedTile.value === null) {
+    return;
+  }
+
+  selectedTile.value.victim = newVal;
+});
+watch(black, (newVal, oldVal) => {
+  if (selectedTile.value === null) {
+    return;
+  }
+
+  selectedTile.value.black = newVal;
+});
+watch(bumper, (newVal, oldVal) => {
+  if (selectedTile.value === null) {
+    return;
+  }
+
+  selectedTile.value.bumper = newVal;
+});
+watch(checkpoint, (newVal, oldVal) => {
+  if (selectedTile.value === null) {
+    return;
+  }
+
+  selectedTile.value.checkpoint = newVal;
+});
+watch(startpoint, (newVal, oldVal) => {
+  if (selectedTile.value === null) {
+    return;
+  }
+
+  selectedTile.value.startpoint = newVal;
+});
+watch(ramp, (newVal, oldVal) => {
+  if( selectedTile.value === null) {
+    return;
+  }
+
+  selectedTile.value.isRamp = newVal;
+});
+
+
+function tileClicked(x, y) {
+  selectedTile.value = maze.value[y][x];
+  victim.value = selectedTile.value.victim;
+  black.value = selectedTile.value.black;
+  bumper.value = selectedTile.value.bumper;
+  checkpoint.value = selectedTile.value.checkpoint;
+  startpoint.value = selectedTile.value.startpoint;
+  ramp.value = selectedTile.value.isRamp;
+}
 
 function back() {
   router.back()
-}
-
-function tileClicked(x, y) {
-  console.log(x, y)
 }
 </script>
