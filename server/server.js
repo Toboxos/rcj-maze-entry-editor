80d28abe675de565ace6f0d7fc86850a8c0c6e35
Tobias Bungard <tobi-bungard@t-online.de>
2023-11-15 22:12:58 +0100

    Updated database format and implemented rest functions

--- server/server.js
+++ server/server.js
@@ -42,12 +42,81 @@ app.put("/parcour/:id", (req, res) => {
 
 app.delete("/parcour/:id", (req, res) => {
     const id = req.params.id
-    console.log("delete parcour "+ id)
 
     db.run("DELETE FROM parcours WHERE id = ?", id, (err, _) => {
         if( err === null ) {
             res.send({})
         }
+    })
+})
+
+app.get("/competitions", (req, res) => {
+    db.all(`SELECT * FROM competitions`, (err, rows) => {
+        res.send(rows)
+    })
+})
+
+app.post("/competition", (req, res) => {
+    const data = req.body
+    const name = data.name
+
+    db.run("INSERT INTO competitions (name) VALUES (?)", [name], function (err, result) {
+        res.send({})
+    })
+})
+
+app.delete("/competition/:id", (req, res) => {
+    const id = req.params.id
+
+    db.run("DELETE FROM competitions WHERE id = ?", [id], function (err, result) {
+        res.send({})
+    })
+})
+
+app.get("/competition/:id/teams", (req, res) => {
+    const id = req.params.id
+    db.all(`SELECT * FROM teams WHERE competition = ?`, (err, rows) => {
+        res.send(rows)
+    })
+})
+
+app.post("/competition/:id/team", (req, res) => {
+    const id = req.params.id
+    const data = req.body
+    const name = data.name
+
+    db.run("INSERT INTO teams (name, competition) VALUES (?, ?)", [name, id], function (err, result) {
+        res.send({})
+    })
+})
+
+app.delete("/competition/:comp/team/:id", (req, res) => {
+    const id = req.params.id
+
+    db.run("DELETE FROM teams WHERE id = ?", [id], function (err, result) {
+        res.send({})
+    })
+})
+
+app.get("/competition/:id/schedules", (req, res) => {
+    const id = req.params.id
+    db.all(`SELECT * FROM schedules WHERE competition = ?`, (err, rows) => {
+        res.send(rows)
+    })
+})
+
+app.post("/competition/:id/schedule", (req, res) => {
+    const id = req.params.id
+    const data = req.body
+    const name = data.name
+    const team = data.team
+    const parcour = data.parcour
+    const time = data.time
+
+    db.run("INSERT INTO schedules (name, competition, team, parcour, time, actions) VALUES (?, ?, ?, ?, ?)",
+        [name, id, team, parcour, time, "[]"],
+        function (err, result) {
+            res.send({})
     })
 })
 
--- server/setupDatabase.js
+++ server/setupDatabase.js
@@ -8,5 +8,26 @@ db.exec(`
         name TEXT,
         category TEXT,
         maze TEXT
-    )
+    );
+    
+    CREATE TABLE teams (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        competition INTEGER
+    );
+    
+    CREATE TABLE competitions (
+        id INTEGER PRIMARY KEY,
+        name TEXT
+    );
+    
+    CREATE TABLE schedules (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        competition INTEGER,
+        team INTEGER,
+        parcour INTEGER,
+        time TEXT,
+        actions TEXT
+    );
 `)
