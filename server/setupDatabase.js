5f1941bfdaafe17cb7c9e99b4ed75d71ac6b014f
Tobias Bungard <tobi-bungard@t-online.de>
2023-11-16 11:36:36 +0100

    Added user table to database

--- server/setupDatabase.js
+++ server/setupDatabase.js
@@ -1,8 +1,37 @@
 import sqlite3 from 'sqlite3'
-sqlite3.verbose()
-const db = new sqlite3.Database('database.db')
+import * as readline from "readline";
+import bcrypt from "bcrypt";
 
-db.exec(`
+console.log("=== Database Setup ===")
+sqlite3.verbose()
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Enter the admin username: ', function(username) {
+    rl.stdoutMuted = true;
+    rl.question('Enter the admin password: ', function(password) {
+        rl.close()
+
+        bcrypt.hash(password, 10, function(err, hash) {
+            if (err) {
+                console.error('Error hashing password:', err);
+                return;
+            }
+
+            const db = new sqlite3.Database('database.db')
+            initDatabase(db)
+            createAdmin(db, username, hash)
+        })
+    })
+})
+
+function initDatabase(db) {
+    console.log(" * Initialize database")
+
+    db.exec(`
     CREATE TABLE parcours (
         id INTEGER PRIMARY KEY,
         name TEXT,
@@ -30,4 +59,21 @@ db.exec(`
         actions TEXT,
         score INTEGER
     );
+    
+    CREATE TABLE users (
+        id INTEGER PRIMARY KEY,
+        username TEXT,
+        password TEXT,
+        permission_level INTEGER
+    );
 `)
+}
+
+function createAdmin(db, username, password) {
+    console.log(" * Create admin user " + username)
+    db.run(`
+        INSERT INTO users (username, password, permission_level)
+        VALUES (?, ?, ?)
+    `, [username, password, 2])
+}
+
