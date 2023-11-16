a9f38977bec3f833ab99c28441b9d907d629bd2a
Tobias Bungard <tobi-bungard@t-online.de>
2023-11-16 12:49:58 +0100

    Added login to vue app

--- src/api.js
+++ src/api.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const instance = axios.create({baseURL: import.meta.env.VITE_API_URL})
+const instance = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true
+})
 
 export const API = {
     axios: instance
